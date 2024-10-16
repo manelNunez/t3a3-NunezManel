0cd2728f692781a85b5d75a54f402caa0f71f319
manelNunez <manel.nunezfer@gmail.com>
2024-10-16 17:27:44 +0200

    activity done

--- app/src/main/java/com/example/t3a3_nunezmanel/MainActivity.kt
+++ app/src/main/java/com/example/t3a3_nunezmanel/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.t3a3_nunezmanel
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.t3a3_nunezmanel.databinding.ActivityLoginBinding
+import com.example.t3a3_nunezmanel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val usuario_tv = binding.usuarioInfoTv
+
+        usuario_tv.text = intent.getStringExtra("Usuario")
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
